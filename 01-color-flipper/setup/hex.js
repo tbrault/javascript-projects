ba3d2d2bbe1af4069d62480b5e35a91421815ae7
tbrault <teddy.brault@mythalesgroup.io>
2022-04-12 12:33:17 +0200

    Refactor + add 03-review

--- 01-color-flipper/setup/hex.js
+++ 01-color-flipper/setup/hex.js
@@ -2,16 +2,22 @@ const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
 const btn = document.querySelector("#btn");
 const color = document.querySelector(".color");
 
-const getRandomColor = () => {
+const getRandomIndice = () => {
+  return Math.floor(Math.random() * hex.length);
+};
+
+const getRandomHexColor = () => {
   let hexColor = "#";
   for (let i = 0; i < 6; i++) {
-    hexColor += hex[Math.floor(Math.random() * hex.length)];
+    hexColor += hex[getRandomIndice()];
   }
   return hexColor;
 };
 
-btn.addEventListener("click", () => {
-  const randomColor = getRandomColor();
+const changeBackgroundColor = () => {
+  const randomColor = getRandomHexColor();
   document.body.style.backgroundColor = randomColor;
   color.textContent = randomColor;
-});
+};
+
+btn.addEventListener("click", changeBackgroundColor);
